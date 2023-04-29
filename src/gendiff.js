c0042f6964a0a0392f7c6f85ce2be2088ce190e6
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-04-29 15:09:27 +0800

    changes

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,31 +1,120 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
-export default (pathOne = filePathOne, pathTwo = filePathTwo) => {
-	const internalTree = buildTree(pathOne, pathTwo);
-	return format(internalTree);
-};
+import _ from 'lodash';
 
-const format = (internalTree) => {
-	const [objectOne, objectTwo] = internalTree;
-	const rawResult = [];
-	for (const [key, value] of Object.entries(objectOne)) {
-		if (Object.hasOwn(objectTwo, key) && objectOne[key] === objectTwo[key]) {
-			rawResult.push(`  ${key}: ${value} \n`);
-		} else if (!Object.hasOwn(objectTwo, key)) {
-			rawResult.push(`- ${key}: ${value} \n`);
-		} else if (Object.hasOwn(objectTwo, key) && objectOne[key] !== objectTwo[key]) {
-			rawResult.push(`- ${key}: ${value} \n`);
-			rawResult.push(`+ ${key}: ${objectTwo[key]} \n`);
-		}
+export default (pathOne = filePathOne, pathTwo = filePathTwo, formatName = 'stylysh') => {
+	const innerTree = buildTree(pathOne, pathTwo);
+	return format(innerTree);
+};
+const getLevelIndent = (level) => {
+	const indent = '11';
+	let levelIndent = '';
+	do {
+		levelIndent += indent;
+		level -= 1;
+	} while (level !== 0);
+	console.log(levelIndent)
+	return levelIndent;
+}
+
+const format = (innerTree) => {
+	let result;
+	let list = [];
+
+	const printSimpleFlatList = (obj, level) => {
+		level += 1;
+		let indent = _.repeat('    ', level);
+		Object.entries(obj).map(([key, value]) => {
+			if (typeof value === 'object') {
+				list.push(`${indent}  ${key}: { \n`);
+				printSimpleFlatList(value, level);
+				list.push(`${indent}  }\n`);
+			} else {
+				list.push(`${indent}  ${key}: ${value} \n`);
+			}
+		});
+		return list;
 	}
-	for (const [key, value] of Object.entries(objectTwo)) {
-		if (!Object.hasOwn(objectOne, key)) {
-			rawResult.push(`+ ${key}: ${value} \n`);
+	const getAllUniquePropsList = (innerTree) => {
+		let allProps = [];
+		for (let obj of innerTree) {
+			allProps.push(Object.keys(obj));
 		}
+		const props = allProps.flat();
+		return _.uniqBy(props,JSON.stringify).sort();
 	}
-	const finalResultString = rawResult.join('');
-	console.log(finalResultString);
-	return finalResultString;
+
+	const printResult = (innerTree, level = 0) => {
+		const [objectOne, objectTwo] = innerTree;
+		level++;
+		let indent = _.repeat('    ', level);
+		const props = getAllUniquePropsList(innerTree);
+		props.map((key) => {
+
+			if (_.has(objectOne, key) && _.has(objectTwo, key)) {
+				if (objectOne[key] === null ||
+						objectTwo[key] === null ||
+						typeof objectOne[key] !== 'object' &&
+						typeof objectTwo[key] !== 'object') {
+							if (objectOne[key] === objectTwo[key]) {
+								list.push(`${indent}  ${key}: ${objectOne[key]}\n`);
+							}
+							if (objectOne[key] !== objectTwo[key]) {
+								list.push(`${indent}- ${key}: ${objectOne[key]}\n`);
+								list.push(`${indent}+ ${key}: ${objectTwo[key]}\n`);
+							}
+				}
+				if (typeof objectOne[key] === 'object' &&
+						typeof objectTwo[key] === 'object') {
+					list.push(`${indent} ${key}: {\n`);
+					printResult([objectOne[key],objectTwo[key]], level);
+					list.push(`${indent}  }\n`);
+				}
+			}
+			if (!_.has(objectOne, key)) {
+				if (objectTwo[key] !== null && typeof objectTwo[key] === 'object') {
+					list.push(`${indent}+ ${key}: {\n`);
+					printSimpleFlatList(objectTwo[key], level);
+					list.push(`${indent}  }\n`);
+				}
+				if (objectTwo[key] === null || typeof objectTwo[key] !== 'object') {
+					list.push(`${indent}+ ${key}: ${objectTwo[key]} \n`);
+				}
+			}
+			if (!_.has(objectTwo, key)) {
+				if (objectTwo[key] !== null && typeof objectOne[key] === 'object') {
+					list.push(`${indent}- ${key}: {\n`);
+					printSimpleFlatList(objectOne[key], level);
+					list.push(`${indent}  }\n`);
+				}
+				if (objectTwo[key] === null || typeof objectOne[key] !== 'object') {
+					list.push(`${indent}- ${key}: ${objectOne[key]} \n`);
+				}
+			}
+
+		});
+		// 1. Если оба объекта имеют ключ
+		// 		1.1. и значения ключей не являются объектами
+		// 				 1.1.1. а также значения ключей равны
+		//				 1.1.2. значения ключей не равны
+		// 		1.2. если оба объекта являются объектами
+		// 		1.3. если значение первого ключа является объектом
+		// 		1.4. если значение второго ключа является объектом
+		// 2. Если первый объект не имеет ключа
+		//		2.1. и если ключ не является объектом
+		// 		2.2. и если ключ является объектом
+		// 3. Если второй объект не имеет ключа
+		// 		3.1. и если ключ не является объектом
+		// 		3.2. и если ключ является объектом
+
+	}
+	list.push(`{ \n`);
+	printResult(innerTree);
+	list.push(`}`);
+
+	result = list.join('');
+	console.log(result);
+	return result;
 }
 const buildTree = (pathOne, pathTwo) => {
 	const objectOne = getData(pathOne);
@@ -53,4 +142,3 @@ const readFile = (file) => {
 	});
 };
 const [, , filePathOne, filePathTwo] = process.argv;
-// export default genDiff;
--- src/gendiff_2.js
+++ src/gendiff_2.js
@@ -1,84 +1,40 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
-import _ from 'lodash';
-
-export default (pathOne = filePathOne, pathTwo = filePathTwo, formatName = 'stylysh') => {
-	const internalTree = buildTree(pathOne, pathTwo);
-	return format(internalTree);
-};
-const getLevelIndent = (level) => {
-	const indent = '11';
-	let levelIndent = '';
-	do {
-		levelIndent += indent;
-		level -= 1;
-	} while (level !== 0);
-	console.log(levelIndent)
-	return levelIndent;
-}
-
-const format = ([objectOne, objectTwo]) => {
-	let result;
-	const genFlatResultList = (objectOne, objectTwo) => {
-		const list = [];
-		const printResult = (objectOne, objectTwo, indent = '') => {
-			indent += '  ';
-			Object.entries(objectOne).map(([key, value]) => {
-				if (typeof value !== 'object') {
-					if (Object.hasOwn(objectTwo, key) && objectOne[key] === objectTwo[key]) {
-						list.push(`${indent}  ${key}: ${value} \n`);
-					} else if (!Object.hasOwn(objectTwo, key)) {
-						list.push(`${indent}- ${key}: ${value} \n`);
-					} else if (Object.hasOwn(objectTwo, key) && objectOne[key] !== objectTwo[key]) {
-						list.push(`${indent}- ${key}: ${value} \n`);
-						list.push(`${indent}+ ${key}: ${objectTwo[key]} \n`);
-					}
-				} else if (typeof value === 'object' && _.has(objectTwo, key)) {
-					list.push(`${key}: {\n`);
-					printResult(value, objectTwo[key], indent);
-					list.push(`}\n`);
-				}
-			});
-			Object.entries(objectTwo).map(([key, value]) => {
-				if (!Object.hasOwn(objectOne, key) && typeof value !== 'object') {
-					list.push(`${indent}+ ${key}: ${value} \n`);
-				}
-			});
+{
+	common: {
+		+ follow: false
+		setting1: Value 1
+		- setting2: 200
+		- setting3: true
+		+ setting3: null
+		+ setting4: blah blah
+		+ setting5: {
+			key5: value5
 		}
-		list.push(`{ \n`);
-		printResult(objectOne, objectTwo );
-		list.push(`}`);
-		return list.join('');
+		setting6: {
+			doge: {
+				- wow:
+				+ wow: so much
+			}
+			key: value
+			+ ops: vops
+		}
 	}
-
-
-	result = genFlatResultList(objectOne, objectTwo);
-	console.log(result);
-	return result;
+	group1: {
+		- baz: bas
+		+ baz: bars
+		foo: bar
+	}
+	- group2: {
+	abc: 12345
+	deep: {
+		id: 45
+	}
 }
-const buildTree = (pathOne, pathTwo) => {
-	const objectOne = getData(pathOne);
-	const objectTwo = getData(pathTwo);
-	return [objectOne, objectTwo];
-}
-const getData = (file) => {
-    const fileFormat = getFileFormat(file);
-    if (fileFormat === 'json') {
-        return JSON.parse(readFile(file));
-    } else if (fileFormat === 'yaml') {
-				return yaml.load(readFile(file));
-    }
-}
-const getFileFormat = (filePath) => {
-	const [, fileFormat] = filePath.split('.');
-	if (fileFormat === 'json') { return 'json' }
-	else if ('yml' || 'yaml') { return 'yaml' }
-}
-const readFile = (file) => {
-	return fs.readFileSync(file, 'utf8', (err) => {
-		if (err) {
-			console.error(err);
+	+ group3: {
+	deep: {
+		id: {
+			number: 45
 		}
-	});
-};
-const [, , filePathOne, filePathTwo] = process.argv;
+	}
+	fee: 100500
+}
+}
