0f7f71e1c12f6c7e994f812385552a0e139fee9f
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-05-30 16:27:57 +0800

    add formatter condition

--- src/gendiff.js
+++ src/gendiff.js
@@ -45,103 +45,104 @@ const getLevelParams = (level) => {
   return { newLevel, indent };
 };
 
-const format = (innerTree) => {
+const format = (innerTree, formatStyle) => {
   let result = '';
   const list = [];
+  if (formatStyle === 'stylish') {
+    const printSimpleFlatList = (obj, level) => {
+      const { newLevel, indent } = getLevelParams(level);
 
-  const printSimpleFlatList = (obj, level) => {
-    const { newLevel, indent } = getLevelParams(level);
+      Object.entries(obj).map(([key, value]) => {
+        if (typeof value === 'object') {
+          list.push(`${indent}${key}: { \n`);
+          printSimpleFlatList(value, newLevel);
+          list.push(`${indent}}\n`);
+        } else {
+          list.push(`${indent}${key}: ${value} \n`);
+        }
+        return list;
+      });
+    };
 
-    Object.entries(obj).map(([key, value]) => {
-      if (typeof value === 'object') {
-        list.push(`${indent}${key}: { \n`);
-        printSimpleFlatList(value, newLevel);
-        list.push(`${indent}}\n`);
-      } else {
-        list.push(`${indent}${key}: ${value} \n`);
+    const getAllUniquePropsList = (tree) => {
+      const allProps = [];
+      for (const obj of tree) {
+        allProps.push(Object.keys(obj));
       }
-      return list;
-    });
-  };
+      const props = allProps.flat();
+      return _.uniqBy(props, JSON.stringify).sort();
+    };
 
-  const getAllUniquePropsList = (tree) => {
-    const allProps = [];
-    for (const obj of tree) {
-      allProps.push(Object.keys(obj));
-    }
-    const props = allProps.flat();
-    return _.uniqBy(props, JSON.stringify).sort();
-  };
-
-  const printResult = (tree, level = 0) => {
-    const [objectOne, objectTwo] = tree;
-    const { newLevel, indent } = getLevelParams(level);
-    const props = getAllUniquePropsList(tree);
-    props.map((key) => {
-      if (_.has(objectOne, key) && _.has(objectTwo, key)) {
-        if (objectOne[key] === null
+    const printResult = (tree, level = 0) => {
+      const [objectOne, objectTwo] = tree;
+      const { newLevel, indent } = getLevelParams(level);
+      const props = getAllUniquePropsList(tree);
+      props.map((key) => {
+        if (_.has(objectOne, key) && _.has(objectTwo, key)) {
+          if (objectOne[key] === null
             || objectTwo[key] === null
             || (typeof objectOne[key] !== 'object'
-            && typeof objectTwo[key] !== 'object')) {
-          if (objectOne[key] === objectTwo[key]) {
-            list.push(`${indent}  ${key}: ${objectOne[key]}\n`);
+              && typeof objectTwo[key] !== 'object')) {
+            if (objectOne[key] === objectTwo[key]) {
+              list.push(`${indent}  ${key}: ${objectOne[key]}\n`);
+            }
+            if (objectOne[key] !== objectTwo[key]) {
+              list.push(`${indent}- ${key}: ${objectOne[key]}\n`);
+              list.push(`${indent}+ ${key}: ${objectTwo[key]}\n`);
+            }
           }
-          if (objectOne[key] !== objectTwo[key]) {
-            list.push(`${indent}- ${key}: ${objectOne[key]}\n`);
+          if (typeof objectOne[key] === 'object'
+            && typeof objectTwo[key] === 'object') {
+            list.push(`${indent} ${key}: {\n`);
+            printResult([objectOne[key], objectTwo[key]], newLevel);
+            list.push(`${indent}}\n`);
+          }
+          if (typeof objectOne[key] === 'object'
+            && typeof objectTwo[key] !== 'object') {
+            list.push(`${indent}- ${key}: {\n`);
+            printSimpleFlatList(objectOne[key], newLevel);
+            list.push(`${indent}}\n`);
             list.push(`${indent}+ ${key}: ${objectTwo[key]}\n`);
           }
         }
-        if (typeof objectOne[key] === 'object'
+        if (!_.has(objectOne, key)) {
+          if (objectTwo[key] !== null
             && typeof objectTwo[key] === 'object') {
-          list.push(`${indent} ${key}: {\n`);
-          printResult([objectOne[key], objectTwo[key]], newLevel);
-          list.push(`${indent}}\n`);
-        }
-        if (typeof objectOne[key] === 'object'
-            && typeof objectTwo[key] !== 'object') {
-          list.push(`${indent}- ${key}: {\n`);
-          printSimpleFlatList(objectOne[key], newLevel);
-          list.push(`${indent}}\n`);
-          list.push(`${indent}+ ${key}: ${objectTwo[key]}\n`);
-        }
-      }
-      if (!_.has(objectOne, key)) {
-        if (objectTwo[key] !== null
-            && typeof objectTwo[key] === 'object') {
-          list.push(`${indent}+ ${key}: {\n`);
-          printSimpleFlatList(objectTwo[key], newLevel);
-          list.push(`${indent}}\n`);
-        }
-        if (objectTwo[key] === null
+            list.push(`${indent}+ ${key}: {\n`);
+            printSimpleFlatList(objectTwo[key], newLevel);
+            list.push(`${indent}}\n`);
+          }
+          if (objectTwo[key] === null
             || typeof objectTwo[key] !== 'object') {
-          list.push(`${indent}+ ${key}: ${objectTwo[key]}\n`);
+            list.push(`${indent}+ ${key}: ${objectTwo[key]}\n`);
+          }
         }
-      }
-      if (!_.has(objectTwo, key)) {
-        if (objectTwo[key] !== null
+        if (!_.has(objectTwo, key)) {
+          if (objectTwo[key] !== null
             && typeof objectOne[key] === 'object') {
-          list.push(`${indent}- ${key}: {\n`);
-          printSimpleFlatList(objectOne[key], newLevel);
-          list.push(`${indent}}\n`);
-        }
-        if (objectTwo[key] === null
+            list.push(`${indent}- ${key}: {\n`);
+            printSimpleFlatList(objectOne[key], newLevel);
+            list.push(`${indent}}\n`);
+          }
+          if (objectTwo[key] === null
             || typeof objectOne[key] !== 'object') {
-          list.push(`${indent}- ${key}: ${objectOne[key]}\n`);
+            list.push(`${indent}- ${key}: ${objectOne[key]}\n`);
+          }
         }
-      }
-      return list;
-    });
-  };
-  list.push('{\n');
-  printResult(innerTree);
-  list.push('}');
+        return list;
+      });
+    };
+    list.push('{\n');
+    printResult(innerTree);
+    list.push('}');
 
-  result = list.join('');
-  console.log(result);
+    result = list.join('');
+    console.log(result);
+  }
   return result;
 };
 
-export default (pathOne = filePathOne, pathTwo = filePathTwo) => {
+export default (pathOne = filePathOne, pathTwo = filePathTwo, formatStyle = 'stylish') => {
   const innerTree = buildTree(pathOne, pathTwo);
-  return format(innerTree);
+  return format(innerTree, formatStyle);
 };
