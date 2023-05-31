4ebf540584bcc5805b25046fac77da9da16ac276
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-05-31 20:29:03 +0800

    add commander options

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,9 +1,24 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
-import _ from 'lodash';
-import stylishFormatter from './formatters/stylish.js'
+import stylishFormatter from './formatters/stylish.js';
+import {Command} from "commander";
+import metaData from "../package.json" assert {type: 'json'};
 
-const [, , filePathOne, filePathTwo] = process.argv;
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('  Compares two configuration files and shows a difference.\n')
+  .version(metaData.version);
+program
+  .option('-f, --format <type>', 'output format')
+program.parse();
+
+const options = program.opts();
+if (options.format) console.log(options.format);
+const formatStyle = options.format;
+
+export const [, , filePathOne, filePathTwo] = process.argv;
 
 const readFile = (file) => {
   return fs.readFileSync(file, 'utf8', (err) => {
@@ -23,14 +38,14 @@ const getData = (file) => {
   let data;
 
   switch (fileFormat) {
-  case 'json':
-    data = JSON.parse(readFile(file));
-    break;
-  case 'yaml':
-    data = yaml.load(readFile(file));
-    break;
-  default:
-    console.log('File format undefined');
+    case 'json':
+      data = JSON.parse(readFile(file));
+      break;
+    case 'yaml':
+      data = yaml.load(readFile(file));
+      break;
+    default:
+      console.log('File format undefined');
   }
   return data;
 };
@@ -41,15 +56,20 @@ const buildTree = (pathOne, pathTwo) => {
   return [objectOne, objectTwo];
 };
 
-const format = (innerTree, formatStyle) => {
+const printResult = (innerTree, formatStyle) => {
   let result;
-  if (formatStyle === 'stylish') {
-    result = stylishFormatter(innerTree);
+  switch (formatStyle) {
+    case 'stylish':
+      return result = stylishFormatter(innerTree);
+      break;
+    default:
+      console.log('Result Format undifined.');
   }
   return result;
 };
 
-export default (pathOne = filePathOne, pathTwo = filePathTwo, formatStyle = 'stylish') => {
+
+export default (pathOne = filePathOne, pathTwo = filePathTwo, style = formatStyle) => {
   const innerTree = buildTree(pathOne, pathTwo);
-  return format(innerTree, formatStyle);
+  return printResult(innerTree, style);
 };
