bdffec949e6934b630f65274849052fbac5ed2a2
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-06-24 16:02:39 +0800

    add refactored stylish format

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,14 +1,12 @@
 import _ from 'lodash';
 
-const getLevelParams = (level) => {
-  let newLevel = level; newLevel += 1;
-  const indent = _.repeat('    ', newLevel);
-  return { newLevel, indent };
-};
+const getLevel = (level) => { return level += 1 };
+const getIndent = (newLevel) => { return _.repeat('    ', newLevel) };
 const printSimpleFlatList = (obj, level) => {
-  const { newLevel, indent } = getLevelParams(level);
+  const newLevel = getLevel(level);
+  const indent = getIndent(newLevel);
   const list = [];
-  console.log(Object.entries(obj));
+
   for (const [key, value] of Object.entries(obj)) {
     if (value !== null && typeof value === 'object') {
       list.push(`${indent}  ${key}: { \n`);
@@ -18,62 +16,62 @@ const printSimpleFlatList = (obj, level) => {
       list.push(`${indent}  ${key}: ${value} \n`);
     }
   }
+
   return _.flatten(list).join('');
 };
 
+const mapping = {
+  nested: (children, key, newLevel, indent) => `${indent}  ${key}: {\n${printResult(children, newLevel)}${indent}  }\n`,
+  added: {
+    obj: (value, key, newLevel, indent) => `${indent}+ ${key}: {\n${printSimpleFlatList(value, newLevel)}${indent}  }\n`,
+    nonObj: (value, key, indent) => `${indent}+ ${key}: ${value}\n`
+  },
+  deleted: {
+    obj: (value, key, newLevel, indent) => `${indent}- ${key}: {\n${printSimpleFlatList(value, newLevel)}${indent}  }\n`,
+    nonObj: (value, key, indent) => `${indent}- ${key}: ${value}\n`
+  },
+  changed: {
+    obj: (value, key, newLevel, indent) => `${indent}- ${key}: {\n${printSimpleFlatList(value, newLevel)}${indent}  }\n`,
+    nonObj: (value, key, indent) => `${indent}- ${key}: ${value}\n`
+  },
+  unchanged: {
+    nonObj: (value, key, indent) => `${indent}  ${key}: ${value}\n`
+  },
+}
+
 const printResult = (diff, level = 0) => {
   const list = [];
+  const newLevel = getLevel(level);
+
   diff.map(obj => {
-    const { newLevel, indent } = getLevelParams(level);
+    const indent = getIndent(newLevel);
     const { key, value, value1, value2, type, children } = obj;
 
     if (type === 'nested') {
-      list.push(`${indent}  ${key}: {\n`);
-      list.push(printResult(children, newLevel));
-      list.push(`${indent}  }\n`);
+      list.push(mapping[type](children, key, newLevel, indent));
     }
     if (type === 'added') {
-      if (value !== null && typeof value === 'object') {
-        list.push(`${indent}+ ${key}: {\n`);
-        list.push(printSimpleFlatList(value, newLevel));
-        list.push(`${indent}  }\n`);
-      } else {
-        list.push(`${indent}+ ${key}: ${value}\n`);
-      }
+      if (value !== null && typeof value === 'object') list.push(mapping[type]['obj'](value, key, newLevel, indent));
+      else list.push(mapping[type]['nonObj'](value, key, indent));
     }
     if (type === 'changed') {
-      if (value1 !== null && typeof value1 === 'object') {
-        list.push(`${indent}- ${key}: {\n`);
-        list.push(printSimpleFlatList(value1, newLevel));
-        list.push(`${indent}  }\n`);
-      } else {
-        list.push(`${indent}- ${key}: ${value1}\n`);
-      }
-      if (value2 !== null && typeof value2 === 'object') {
-        list.push(`${indent}+ ${key}: {\n`);
-        list.push(printSimpleFlatList(value2, newLevel));
-        list.push(`${indent}  }\n`);
-      } else {
-        list.push(`${indent}+ ${key}: ${value2}\n`);
-      }
+      if (value1 !== null && typeof value1 === 'object') list.push(mapping['deleted']['obj'](value1, key, newLevel, indent));
+      else list.push(mapping['deleted']['nonObj'](value1, key, indent));
+
+      if (value2 !== null && typeof value2 === 'object') list.push(mapping['added']['obj'](value2, key, newLevel, indent));
+      else list.push(mapping['added']['nonObj'](value2, key, indent));
     }
     if (type === 'deleted') {
-      if (value !== null && typeof value === 'object') {
-        list.push(`${indent}- ${key}: {\n`);
-        list.push(printSimpleFlatList(value, newLevel));
-        list.push(`${indent}  }\n`);
-      } else {
-        list.push(`${indent}- ${key}: ${value}\n`);
-      }
+      if (value !== null && typeof value === 'object') list.push(mapping[type]['obj'](value, key, newLevel, indent));
+      else list.push(mapping[type]['nonObj'](value, key, indent));
     }
-    if (type === 'unchanged') {
-      list.push(`${indent}  ${key}: ${value}\n`);
-    }
+    if (type === 'unchanged') list.push(mapping[type]['nonObj'](value, key, indent));
+
   });
+
   return _.flatten(list).join('');
 }
-const printStylishFormat = (diff) => {
+export default (diff) => {
   const result = printResult(diff);
   return `{\n${result}}`;
 }
-export default printStylishFormat;
