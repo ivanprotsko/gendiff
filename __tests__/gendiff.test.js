a565f26d0088aa61aae30f6ac3d9f037c0076084
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-06-29 13:36:33 +0800

    add eslint fixes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,68 +1,98 @@
 import path from 'path';
-import gendiff from '../src/gendiff.js'"';
+import gendiff from '../src/gendiff.js';
 import result from '../__fixtures__/expected-formats.js';
 
 const buildPath = (format, structure, file) => {
   const __dirname = path.resolve();
   return `${__dirname}/__fixtures__/files/${format}/${structure}/${file}.${format}`;
-}
+};
 test('Stylish, YAML, flat', () => {
-  expect(gendiff(
-    buildPath('yaml', 'flat', 'a'),
-    buildPath('yaml', 'flat', 'b'),
-    'stylish')).toEqual(result.stylish.flat);
+  expect(
+    gendiff(
+      buildPath('yaml', 'flat', 'a'),
+      buildPath('yaml', 'flat', 'b'),
+      'stylish',
+    ),
+  ).toEqual(result.stylish.flat);
 });
 test('Stylish, YAML, nested', () => {
-  expect(gendiff(
-    buildPath('yaml', 'nested', 'a'),
-    buildPath('yaml', 'nested', 'b'),
-    'stylish')).toEqual(result.stylish.nested);
+  expect(
+    gendiff(
+      buildPath('yaml', 'nested', 'a'),
+      buildPath('yaml', 'nested', 'b'),
+      'stylish',
+    ),
+  ).toEqual(result.stylish.nested);
 });
 test('Stylish, YML, flat', () => {
-  expect(gendiff(
-    buildPath('yml', 'flat', 'a'),
-    buildPath('yml', 'flat', 'b'),
-    'stylish')).toEqual(result.stylish.flat);
+  expect(
+    gendiff(
+      buildPath('yml', 'flat', 'a'),
+      buildPath('yml', 'flat', 'b'),
+      'stylish',
+    ),
+  ).toEqual(result.stylish.flat);
 });
 test('Stylish, YML, nested', () => {
-  expect(gendiff(
-    buildPath('yml', 'nested', 'a'),
-    buildPath('yml', 'nested', 'b'),
-    'stylish')).toEqual(result.stylish.nested);
+  expect(
+    gendiff(
+      buildPath('yml', 'nested', 'a'),
+      buildPath('yml', 'nested', 'b'),
+      'stylish',
+    ),
+  ).toEqual(result.stylish.nested);
 });
 test('Stylish, JSON, flat', () => {
-  expect(gendiff(
-    buildPath('json', 'flat', 'a'),
-    buildPath('json', 'flat', 'b'),
-    'stylish')).toEqual(result.stylish.flat);
+  expect(
+    gendiff(
+      buildPath('json', 'flat', 'a'),
+      buildPath('json', 'flat', 'b'),
+      'stylish',
+    ),
+  ).toEqual(result.stylish.flat);
 });
 test('Stylish, JSON, nested', () => {
-  expect(gendiff(
-    buildPath('json', 'nested', 'a'),
-    buildPath('json', 'nested', 'b'),
-    'stylish')).toEqual(result.stylish.nested);
+  expect(
+    gendiff(
+      buildPath('json', 'nested', 'a'),
+      buildPath('json', 'nested', 'b'),
+      'stylish',
+    ),
+  ).toEqual(result.stylish.nested);
 });
 test('Plain, JSON, nested', () => {
-  expect(gendiff(
-    buildPath('json', 'nested', 'a'),
-    buildPath('json', 'nested', 'b'),
-    'plain')).toEqual(result.plain.nested);
+  expect(
+    gendiff(
+      buildPath('json', 'nested', 'a'),
+      buildPath('json', 'nested', 'b'),
+      'plain',
+    ),
+  ).toEqual(result.plain.nested);
 });
 test('Plain, JSON, flat', () => {
-  expect(gendiff(
-    buildPath('json', 'flat', 'a'),
-    buildPath('json', 'flat', 'b'),
-    'plain')).toEqual(result.plain.flat);
+  expect(
+    gendiff(
+      buildPath('json', 'flat', 'a'),
+      buildPath('json', 'flat', 'b'),
+      'plain',
+    ),
+  ).toEqual(result.plain.flat);
 });
 test('JSON, JSON, nested', () => {
-  expect(gendiff(
-    buildPath('json', 'nested', 'a'),
-    buildPath('json', 'nested', 'b'),
-    'json')).toEqual(result.json.nested);
+  expect(
+    gendiff(
+      buildPath('json', 'nested', 'a'),
+      buildPath('json', 'nested', 'b'),
+      'json',
+    ),
+  ).toEqual(result.json.nested);
 });
 test('JSON, JSON, flat', () => {
-  expect(gendiff(
-    buildPath('json', 'flat', 'a'),
-    buildPath('json', 'flat', 'b'),
-    'json')).toEqual(result.json.flat);
+  expect(
+    gendiff(
+      buildPath('json', 'flat', 'a'),
+      buildPath('json', 'flat', 'b'),
+      'json',
+    ),
+  ).toEqual(result.json.flat);
 });
